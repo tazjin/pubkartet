c8558dc10144b693f89ce24c313fac93efd36524
Vincent Ambo <tazjin@gmail.com>
2016-02-01 16:00:15 +0100

    Prepare for presentation

--- src/API.hs
+++ src/API.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TypeOperators         #-}
 
 -- | Servant API for retrieving pubs
 module API where
@@ -8,6 +11,8 @@ import           Data.Proxy
 import           Data.Text
 import           Pubs
 import           Servant
+import           Servant.API
+import           Servant.Docs
 
 type PubAPI = "pubs" :> Get '[JSON] [Pub]
          :<|> "pubs" :> "tagged"
@@ -16,3 +21,15 @@ type PubAPI = "pubs" :> Get '[JSON] [Pub]
 
 pubAPI :: Proxy PubAPI
 pubAPI = Proxy
+
+-- Documentation instances
+instance ToCapture (Capture "tag" Text) where
+  toCapture _ = DocCapture "tag" "A tag to filter pubs by"
+
+instance ToSample [Pub] [Pub] where
+  toSample _ = Just [samplePub]
+
+samplePub :: Pub
+samplePub = Pub (PubID 53) "BD57 (BrewDog)"
+            (Just "https://untappd.com/venue/2875445")
+            ["godt-utvalg"] (Geography 10.7572369 59.9199413)
